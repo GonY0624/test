cb4e4447852a90b27ab3e5aaad359317560bcec5
chgoeun <goeun990@naver.com>
2024-01-31 13:09:33 +0900

    [test] 프로그램 test

---
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -0,0 +1,26 @@
+package com.ohgiraffers.test;
+
+public class TestApplication {
+
+    //수업목표 오늘의 수업목표입니다.
+    //필기 이것은 필기입니다.
+    //목차 이것은 목차입니다.
+    //여기는 (한 줄)주석입니다.
+    /* 여기는
+    * 여러 줄
+    * 주석 입니다.
+    *  */
+
+    /*
+    * ctrl - space : 자동완성 기능
+    * ctrl - d : 복사
+    * */
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("저는 개발자 입니다.");
+
+    }
+}
